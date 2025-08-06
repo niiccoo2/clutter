4f28e01670a0c67f79fc07630021ddf3068dc2fc
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-06 14:29:16 -0400

    Setup junk at bottom and stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,33 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Londrina+Shadow&family=Orbitron:wght@400;900&display=swap" rel="stylesheet" />
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Londrina+Shadow&family=Nata+Sans:wght@100..900&family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
+
     <title>Clutter - YSWS</title>
 </head>
 
 <style>
-    .Orbitron {
+@import url('https://fonts.googleapis.com/css2?family=Londrina+Shadow&family=Nata+Sans:wght@100..900&family=Orbitron:wght@400..900&display=swap');
+
+.my-londrina {
   font-family: 'Londrina Shadow', cursive;
 }
+
+.my-nata {
+  font-family: 'Nata Sans', sans-serif;
+}
+
+.Orbitron {
+  font-family: 'Orbitron', sans-serif;
+}
+
+.londrina {
+  font-family: 'Londrina Shadow', cursive;
+}
+
+
 .drab-dark-brown {
   color: #2f2504;
 }
@@ -38,25 +58,33 @@
     display: none;
 }
 
+img {
+  position: relative;
+  z-index: -1; 
+}
+
 </style>
 
-<body style="background-color: #594e36;">
+<body style="background-color: #594e36; overflow: hidden;"> <!-- Remove overflow hidden maybe later not for rsvp -->
     <div style="color: #7e846b; width: 102%; padding: 1rem 0; transform: rotate(10deg); box-sizing: border-box;">
-        <h1 class="text-center Orbitron" style="font-size: 8rem; margin: 0;">
-            <p class="rounded-lg inline-block" style="color: d0ddd7; ">Clutter</p>
+        <h1 class="text-center my-nata" style="font-size: 8rem; margin: 0; text-shadow: 2px 2px 4px #000; font-weight: 700;">
+            <span class="rounded-lg inline-block" style="color: #7e846b;">Clutter</span>
         </h1>
     </div>
     
     <div style="position: relative; text-align: center; transform: rotate(-5deg)">
         <img src="images/notebook_paper.png" alt="Notebook paper" style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
         <!-- <h1 class="Orbitron" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 2rem; color: #2f2504; margin: 0;"> -->
-        <h1 class="text-center Orbitron drab-dark-brown-2" style="font-size: 4rem; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -160%);">
-            <a href="/RSVP" class="rounded-lg inline-block" style="text-decoration: none; color: inherit;">RSVP HERE</a>
+        <h1 class="text-center londrina drab-dark-brown-2" style="text-align: center; font-size: 4rem; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -160%); text-shadow: 2px 2px 4px #000; font-weight: 700; width: 80%; max-width: 600px;">
+        <a href="/RSVP" class="rounded-lg inline-block" style="text-decoration: none;">RSVP here!</a>
+        </h1>
+        <h1 class="text-center londrina drab-dark-brown-2" style="font-size: 2rem; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -33%); text-shadow: 2px 2px 4px #000; font-weight: 200; max-width: 600px; text-align: center; overflow-wrap: break-word;">
+        <a href="/RSVP" class="rounded-lg inline-block" style="text-decoration: none; display: block; width: 100%;">You ship an applicaiton<br> that cleans up your<br> computer, when ship you <br>something to <br>organize your desk.</a>
         </h1>
     </div>
 
-    <div style="text-align: center; transform: rotate(-5deg)">
-        <img src="images/trash_piles.png" alt="Clutter at the bottom" style="max-width: 100%; height: auto; display: block; margin: 0 auto; overflow: hidden; margin-top: -10rem">
+    <div style="text-align: center; transform: rotate(0deg); margin-top: -20vh">
+        <img src="images/trash_piles.png" alt="Clutter at the bottom" style="max-width: 100%; height: auto; display: block; margin: 0 auto; overflow: hidden">
     </div>
 
 
