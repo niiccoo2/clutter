3020f3bc6615257168ea0690173adf9578413a8d
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-06 12:57:32 -0400

    Made stuff work its really ugly tho so ima fix that later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Londrina+Shadow&family=Orbitron:wght@400;900&display=swap" rel="stylesheet" />
+
+    <title>Clutter - YSWS</title>
+</head>
+
+<style>
+    .Orbitron {
+  font-family: 'Londrina Shadow', cursive;
+}
+.drab-dark-brown {
+  color: #2f2504;
+}
+
+.drab-dark-brown-2 {
+  color: #594e36; 
+}
+
+.reseda-green {
+  color: #7e846b; 
+}
+
+.reseda-green2 {
+  color: #afb892; 
+}
+
+.ash-gray {
+  color: #a5ae9e; 
+}
+
+.platinum {
+  color: #d0ddd7; 
+}
+
+.hidden {
+    display: none;
+}
+
+</style>
+
+<body style="background-color: #594e36;">
+    <div style="color: #7e846b; width: 102%; padding: 1rem 0; transform: rotate(10deg); box-sizing: border-box;">
+        <h1 class="text-center Orbitron" style="font-size: 8rem; margin: 0;">
+            <p class="rounded-lg inline-block" style="color: d0ddd7; ">Clutter</p>
+        </h1>
+    </div>
+    
+    <div style="position: relative; text-align: center; transform: rotate(-5deg)">
+        <img src="images/notebook_paper.png" alt="Notebook paper" style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
+        <!-- <h1 class="Orbitron" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 2rem; color: #2f2504; margin: 0;"> -->
+        <h1 class="text-center Orbitron drab-dark-brown-2" style="font-size: 4rem; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -160%);">
+            <a href="/RSVP" class="rounded-lg inline-block" style="text-decoration: none; color: inherit;">RSVP HERE</a>
+        </h1>
+    </div>
+
+
+    <!-- <div style="text-align: center;">
+        <h1 class="text-center Orbitron" style="font-size: 4rem;">
+            <a href="/RSVP" class="rounded-lg inline-block">RSVP HERE</a>
+        </h1>
+    </div>
+
+    <img src="images/notebook_paper.png" alt="Notebook paper" style="display: block; margin-left: auto; margin-right: auto; max-width: 100%; height: auto;"/> -->
+</body>
---
+++ src/routes/RSVP/+page.svelte
@@ -0,0 +1,6 @@
+<iframe
+  src="https://clutter.fillout.com/rsvp"
+  style="width: 100vw; height: 100vh; border: none;"
+  title="Clutter RSVP Form"
+  allowfullscreen
+></iframe>
