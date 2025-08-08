c2168d3fb87cfd9ab1dea0f16e1f0c0ce0dc6353
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-08 12:05:35 -0400

    Fixed small screen scrolling bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -163,8 +163,8 @@ img {
 
 </style>
 
-<body style="background-color: #594e36; overflow: hidden;">
-    <div style="background-color: #594e36;">
+<body style="background-color: #594e36;">
+    <div style="background-color: #594e36; overflow: hidden;">
         <div style="transform: rotate(10deg); overflow-x: hidden; padding: 1rem 0;">
             <img 
                 src="images/clutter_banner_website.png" 
@@ -173,7 +173,7 @@ img {
             />
         </div>
 
-        <div style="position: relative; text-align: center; transform: rotate(-5deg); overflow-x: hidden;">
+        <div style="position: relative; text-align: center; transform: rotate(-5deg); overflow-x: hidden; overflow: hidden;">
             <img src="images/notebook_paper.png" alt="Notebook paper" style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
             <!-- <h1 class="Orbitron" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 2rem; color: #2f2504; margin: 0;"> -->
             <h1 class="text-center handwritten" style="color: #334195; text-align: center; font-size: 3rem; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -180%); font-weight: 700; width: 80%; max-width: 600px;">
@@ -183,10 +183,10 @@ img {
             <span class="rounded-lg inline-block drab-dark-brown-2" style="text-decoration: none; display: block; width: 100%; line-height: 1.4; ">You ship a script <br/>to organizes your<br> digital life, we ship you <br>something to <br>organize your<br> physical life!</span>
             </h1>
         </div>
-        <div class="text-center p-10 flex justify-center made_with_heart" style="position: relative; z-index: 1; font-weight: 700; color: #7e846b;">
+        <div class="text-center p-10 flex justify-center made_with_heart" style="position: relative; z-index: 1; font-weight: 700; color: #7e846b; overflow: hidden;">
             Made with  <span class="hover:animate-bounce">❤️</span>  by <a href="https://github.com/SuperNinjaCat5" style="margin-left: 0.2vw; margin-right: 0.2vw;"> Ben </a> & <a href="https://github.com/niiccoo2" style="margin-left: 0.2vw; margin-right: 0.2vw;"> Nico </a> 
         </div>  
-        <div class="trash-wrapper trash-image">
+        <div class="trash-wrapper trash-image" style="overflow: hidden;">
           <img src="images/trash_piles.png" alt="Clutter at the bottom">
         </div>
     </div>
